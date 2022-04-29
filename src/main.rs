9d2799d497422d7820fee552a7a9b5bb700942c9
Nigecat <48661288+Nigecat@users.noreply.github.com>
2022-04-29 14:57:45 +1000

    Add test files

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use ffmpeg_video_decoder::VideoDecoder;
+
+fn main() {
+    // let source: &[u8] = include_bytes!("../test.mp4");
+    let source = std::path::PathBuf::from("test.mp4");
+    let mut decoder = VideoDecoder::new(source, false).unwrap();
+
+    // let frame = decoder.next_frame().unwrap().unwrap().into_image();
+    // frame.save("frame1.png").unwrap();
+    // let frame = decoder.next_frame().unwrap().unwrap().into_image();
+    // frame.save("frame2.png").unwrap();
+
+    let mut i = 0;
+    while let Some(frame) = decoder.next_frame().unwrap() {
+        println!("Got frame: {} w/{:?}", i, frame.dimensions());
+
+        i += 1;
+    }
+}
